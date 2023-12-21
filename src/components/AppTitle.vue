527611efac61cdc1269d4b92ca9642d2dcac78d1
EdoBergamo <edo.bergamo03@icloud.com>
2023-12-21 07:37:58 -0800

    [FEAT] Developed Initial Logic

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'titleComponent',
+    data() {
+        return{
+            title: "La mia prima app con Vite!",
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
